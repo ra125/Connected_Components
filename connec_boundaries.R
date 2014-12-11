ad4597717289b6a40fbb06c6683aaf3c2dcd3bd4
Haosheng Luo <hl201@saxon.stat.duke.edu>
2014-12-11 15:32:19 -0500

    revise connec_boundaries

--- connec_boundaries.R
+++ connec_boundaries.R
@@ -1,22 +1,24 @@
 connec_boundaries = function(bw)
 {
   source("connec_label.R")
-  #Check whether the input is valid
+  #Check whether the input is valid.
   stopifnot(is.matrix(bw))
   stopifnot(sum(bw*(1-bw))==0)
   
-  #Label the regions in the image
+  #Label the regions in the image.
   L = connec_label(bw)
   num_labels = max(L)
   
-  #Set up a zeros matrix for saving boundaries later
-  boundary = L
-  boundary[,]=0
+  #Set up a zeros matrix for saving boundaries of the whole image later.
+  BOUNDARY = L
+  BOUNDARY[,]=0
   
-  #Find boundaries for each region
+  #Find boundaries for each region.
   for (n in 1:num_labels){
     segment = L
-    
+    #Set up a zeros matrix for saving boundaries of an individual region.
+    boundary = L
+    boundary[,]=0
     #Subset the region we are looking at. Extract the maxmimum and minimum rows and columns of that region.
     segment[which(!segment==n)]=0
     all.points = which(segment==n, arr.ind=T)
@@ -24,42 +26,52 @@ connec_boundaries = function(bw)
     col.limit = c(min(all.points[,2]), max(all.points[,2]))
     
     #For each row, search the positions where the elements change from zero to the label or vice versa.
-    for (i in row.limit[1]:(row.limit[2])){
-      for(j in col.limit[1]:(col.limit[2])){
+    for (i in row.limit[1]:row.limit[2]){
+      for(j in col.limit[1]:col.limit[2]){
         #When we are in the first column of the image, and if the element is in the region, this is the boundary point. 
         if(j==1 && segment[i,j]==n){
-          boundary[i,j] = n }   
-        #When we are in the last column of the image, and if the element is in the region, this is the boundary point.
-        else if(j==ncol(segment)&&segment[i,j]==n){
-          boundary[i,j] = n  }
-        #When the value of two adjacent elements are different, the bigger one is the boundary point (the background is labeled with zero).
-        else if(segment[i,j]>segment[i,j-1]){
           boundary[i,j] = n
-        }else if (segment[i,j]>segment[i,j+1]){
+        } else if(j==1 && segment[i,j]!=n){
+          boundary[i,j] = 0
+        #When we are in the last column of the image, and if the element is in the region, this is the boundary point.
+        } else if(j==ncol(segment)&&segment[i,j]==n){
+          boundary[i,j] = n
+        } else if(j==ncol(segment)&&segment[i,j]!=n){
+          boundary[i,j] = 0    
+        #When the value of two adjacent elements are different, the bigger one is the boundary point (the background is labeled with zero).
+        } else if(segment[i,j]>segment[i,j-1]){
+          boundary[i,j] = n
+        } else if (segment[i,j]>segment[i,j+1]){
           boundary[i,j] = n
         }
       }
       
     }
     #For each column, search the positions where the elements change from zero to the label or vice versa.
-    for (j in col.limit[1]:(col.limit[2])){
-      for(i in row.limit[1]:(row.limit[2])){
+    for (j in col.limit[1]:col.limit[2]){
+      for(i in row.limit[1]:row.limit[2]){
         #When we are in the first row of the image, and if the element is in the region, this is the boundary point. 
         if(i==1 && segment[i,j]==n){
-          boundary[i,j] = n }
-        #When we are in the last row of the image, and if the element is in the region, this is the boundary point.
-        else if(i==nrow(segment) && segment[i,j]==n){
-          boundary[i,j] = n}
-        #When the value of two adjacent elements are different, the bigger one is the boundary point (the background is labeled with zero).
-        else if(segment[i,j]>segment[i-1,j]){
           boundary[i,j] = n
-        }else if (segment[i,j]>segment[i+1,j]){
+        } else if(i==1 && segment[i,j]!=n){
+          boundary[i,j] = 0
+        #When we are in the last row of the image, and if the element is in the region, this is the boundary point.
+        } else if(i==nrow(segment) && segment[i,j]==n){
+          boundary[i,j] = n
+        } else if(i==nrow(segment) && segment[i,j]!=n){
+          boundary[i,j] = 0
+        #When the value of two adjacent elements are different, the bigger one is the boundary point (the background is labeled with zero).
+        } else if(segment[i,j]>segment[i-1,j]){
+          boundary[i,j] = n
+        } else if (segment[i,j]>segment[i+1,j]){
           boundary[i,j] = n
         }
       } 
-    }    
+    }   
+    #Add all the individual region boundaries.
+    BOUNDARY = BOUNDARY+boundary
   }
-  return(boundary)
+  return(BOUNDARY)
 }
 
 
@@ -71,7 +83,7 @@ bw = matrix(cbind(rbind(matrix(rep(1,25), ncol = 5),
                   rbind(matrix(rep(0,50), ncol = 10),
                         matrix(rep(1,100), ncol = 10),
                         matrix(rep(0, 85*10), ncol =10)),
-                  rbind(matrix(rep(0,100*70), ncol = 70)
+                  rbind(matrix(rep(1,100*70), ncol = 70)
                   ),
                   rbind(matrix(rep(1, 5*10), ncol =10),
                         matrix(rep(0, 80*10), ncol =10),
--- connec_label.R
+++ connec_label.R
@@ -1,57 +1,81 @@
 connec_label=function(image)
 {
+  #Check whether the input is valid.
   stopifnot(is.matrix(image))
   stopifnot(sum(image*(1-image))==0)
   
   m=nrow(image)
   n=ncol(image)
   
+  #Set up a zeros matrix for saving labels later.
   connec=matrix(rep(0,m*n),nrow=m,ncol=n)
-  mark=1
-  diff=1
-  offset=c(-1,m,1,-m)
-  nof=0
   
+  #Initialize the region label. 
+  mark=1
+  
+  #A vector to determine the neighbors.
+  #offset=c(-1,m,1,-m)
+  
+  #The number of 
+  #nof=0
+  
+  #Search by each row and column.
   for(i in 1:m)
   {
     for(j in 1:n)
     {
       if(image[i,j]==1)
       {
-        nof=nof+1
+        #nof=nof+1
+        #Position of the first detected element in the region.
         index=(j-1)*m+i
+        
+        #Label the first element.
         connec[index]=mark
         while(length(index)!=0)
         {
+          #Set visited element equal to zero, that we will not visit again.
           image[index]=0
           neighbors=list()
           for(k in 1:length(index))
           {
+            #If the element is in the bottom of the image, it will only have left, right, and upper neighbors.
             if(index[k]%%m==0 && index[k]!=m*n)
             {
               offset=c(-1,m,-m)
-            } else if(index[k]%%m==1 && index[k]!=(n-1)*m+1)
+            #If the element is in the top of the image, it will only have left, right, and lower neighbors.
+              } else if(index[k]%%m==1 && index[k]!=(n-1)*m+1)
             {
               offset=c(1,m,-m)
+            #If the element is in the very right of the image, it will only have left, upper and lower neighbors.
             } else if(index[k]%/%m==n-1 && index[k]!=(n-1)*m+1 && index[k]!=m*n)
             {
               offset=c(1,-1,-m)
+            #If the element is in the bottom right corner of the image, it will only have left and upper neighbors.
             } else if(index[k]==m*n)
             {
               offset=c(-1,-m)
+            #If the element is in the topright corner of the image, it will only have left and lower neighbors.
             } else if(index[k]==(n-1)*m+1)
             {
               offset=c(1,-m)
+            #Otherwise, the element has all four neighbors.
             } else {offset=c(-1,m,1,-m)}
             
             neighbors=unlist(c(neighbors,index[k]+offset))
           }
           neighbors=unique(neighbors)
+          
+          #Get rid of all the neighbors that are out of the boundaries.
           neighbors=neighbors[neighbors>0]
+          
+          #Extract the position of all the neighbor elements that are in the region and label the element. 
           index=neighbors[which(image[neighbors]==1)]
           connec[index]=mark
         }
-        mark=mark+diff
+        
+        #Update the label before moving to the next region.
+        mark=mark+1
       }
     }
   }
